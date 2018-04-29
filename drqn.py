ec5f393e06bf99737836345c073716aa9f8251c8
nayeemiiitd <mohammad14147@iiitd.ac.in>
2018-04-29 17:06:27 +0530

    Added IOU and gt methods

--- drqn.py
+++ drqn.py
@@ -26,6 +26,41 @@ import itertools as it
 from time import sleep
 import tensorflow as tf
 
+def bb_intersection_over_union(boxA, boxB):
+	# determine the (x, y)-coordinates of the intersection rectangle
+	xA = max(boxA[0], boxB[0])
+	yA = max(boxA[1], boxB[1])
+	xB = min(boxA[2], boxB[2])
+	yB = min(boxA[3], boxB[3])
+ 
+	# compute the area of intersection rectangle
+	interArea = (xB - xA + 1) * (yB - yA + 1)
+ 
+	# compute the area of both the prediction and ground-truth
+	# rectangles
+	boxAArea = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
+	boxBArea = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
+ 
+	# compute the intersection over union by taking the intersection
+	# area and dividing it by the sum of prediction + ground-truth
+	# areas - the interesection area
+	iou = interArea / float(boxAArea + boxBArea - interArea)
+ 
+	# return the intersection over union value
+	return iou
+
+def GT_transform(old_gt,row,col):
+	newGT = np.zeros((old_gt.shape[0],4))
+    x = gt[:,0::2];    
+    y = gt[:,1::2];    
+    newGT[:,0] = np.amin(x,axis=1)
+    newGT[:,1] = np.amin(y,axis=1)
+    newGT[:,2] = np.amax(x,axis=1) - np.amin(x,axis=1)
+    newGT[:,3] = np.amax(y,axis=1) - np.amin(y,axis=1)
+    newGT[:,0] = (newGT[:,0]/col)*224
+    newGT[:,1] = (newGT[:,1]/row)*224
+    return newGT
+
 
 #%%
 
